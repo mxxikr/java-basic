e6cde6cfe6402dea8bbdac89a5005e776d864f9a
mxxikr <mxxikr@gmail.com>
2025-12-19 07:52:51 +0900

    클래스와 데이터: 학생 정보 출력 프로그램 만들기 - 배열 사용

---
+++ src/class1/ClassStart2.java
@@ -0,0 +1,13 @@
+package class1;
+
+class ClassStart2 {
+    public static void main(String[] args) {
+        String[] studentNames = {"학생1", "학생2", "학생3", "학생4"};
+        int[] studentAges = {15, 16, 17, 10, 16};
+        int[] studentGrades = {90, 80,  100, 80, 50};
+
+        for (int i = 0; i < studentNames.length; i++) {
+            System.out.println("이름: " + studentNames[i] + " 나이: " + studentAges[i] + " 성적: " + studentGrades[i]);
+        }
+    }
+}
