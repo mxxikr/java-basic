6ae8c84f887c017ca974a53c1a5fcf6ab7326484
mxxikr <mxxikr@gmail.com>
2025-12-26 11:50:10 +0900

    생성자: this

--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -4,4 +4,11 @@ public class MemberInit {
     String name;
     int age;
     int grade;
+
+    // 추가
+    void initMember(String name, int age, int grade) {
+        this.name = name; // 인스턴스 멤버 변수가 매개변수의 값 사용
+        this.age = age;
+        this.grade = grade;
+    }
 }
