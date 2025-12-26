1088879300ec3586d4fb491fbe3476b006996721
mxxikr <mxxikr@gmail.com>
2025-12-26 12:09:40 +0900

    생성자: 오버로딩과 this()

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -5,6 +5,10 @@ public class MemberConstruct {
     int age;
     int grade;
 
+    MemberConstruct(String name, int age) {
+        this(name, age, 50); // 생성자 내부에서 자신의 생성자 호출 가능
+    }
+
     // 생성자
     MemberConstruct(String name, int age, int grade) { // 생성자 이름은 클래스명과 같아야함
         System.out.println("생성자 호출 name=" + name + ", age=" + age + ", grade=" + grade);
