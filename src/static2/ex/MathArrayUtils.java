482c6e24be47fd28ccdce59980f90000610c28c3
mxxikr <mxxikr@gmail.com>
2025-12-31 10:59:04 +0900

    자바 메모리 구조와 static: 문제 - 수학 유틸리티 클래스

---
+++ src/static2/ex/MathArrayUtils.java
@@ -0,0 +1,40 @@
+package static2.ex;
+
+public class MathArrayUtils {
+
+    MathArrayUtils() {
+        //private 인스턴스 생성을 막음
+    }
+
+    public static int sum(int[] values) {
+        int total = 0;
+        for (int value: values) {
+            total += value;
+        }
+        return total;
+    }
+
+    public static double average(int[] values) {
+        return (double) sum(values) / values.length;
+    }
+
+    public static int min(int[] values) {
+        int min = values[0];
+        for (int value: values) {
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    public static int max(int[] values) {
+        int max = values[0];
+        for (int value: values) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+}
