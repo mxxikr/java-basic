b509f5b33f0fa1aa74af9a2b05b351f2f2c7cd86
mxxikr <mxxikr@gmail.com>
2026-01-02 14:49:59 +0900

    다형성2: 인터페이스

---
+++ src/poly/ex5/Cat.java
@@ -0,0 +1,16 @@
+package poly.ex5;
+
+
+import poly.ex2.Animal;
+
+public class Cat implements InterfaceAnimal {
+    @Override
+    public void sound() {
+        System.out.println("냐옹");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("고양이 이동");
+    }
+}
